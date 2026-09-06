70828849694cffdf1c174191d30066728ade1360
Mar <maryori1990bw@gmail.com>
2026-09-06 17:20:48 -0600

    Update Estudiante.java

--- ejecicioClasepoo/src/ejecicioClasepoo/Estudiante.java
+++ ejecicioClasepoo/src/ejecicioClasepoo/Estudiante.java
@@ -8,7 +8,7 @@ public class Estudiante extends Persona  {
 		creditosObtenidos = creditosObtenidosParam;
 	}
 	
-	@Override
+	@Override //Hace que un método de la clase hija reemplace al método de la clase padre, en vez de usar el original
 	public void imprimirInformacion() {
 		super.imprimirInformacion();
 		System.out.println("creditosObtenidos: " + creditosObtenidos);
